623bf7f8363eb3a8d0ac404355eea50712511240
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-07-01 20:49:12 +0900

    2024-07-01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,9 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("androidx.fragment:fragment:1.3.6")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
--- app/src/main/java/com/example/dailyplanner/MainActivity.java
+++ app/src/main/java/com/example/dailyplanner/MainActivity.java
@@ -1,24 +1,60 @@
 package com.example.dailyplanner;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager2 viewPager;
+    private BottomNavigationView bottomNavBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        viewPager = findViewById(R.id.viewPager);
+        bottomNavBar = findViewById(R.id.bottomNavBar);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        bottomNavBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_home) {
+                    viewPager.setCurrentItem(0);
+                    return true;
+                } else if (id == R.id.nav_settings) {
+                    viewPager.setCurrentItem(1);
+                    return true;
+                } else if (id == R.id.nav_status) {
+                    viewPager.setCurrentItem(2);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                if (position == 0) {
+                    bottomNavBar.getMenu().findItem(R.id.nav_home).setChecked(true);
+                } else if (position == 1) {
+                    bottomNavBar.getMenu().findItem(R.id.nav_settings).setChecked(true);
+                } else if (position == 2) {
+                    bottomNavBar.getMenu().findItem(R.id.nav_status).setChecked(true);
+                }
+            }
         });
     }
 }
